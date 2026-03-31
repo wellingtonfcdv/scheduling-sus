9dbd501f77a941af441d26499806878aa9470821
Wellington Feijo Da Costa <>
2026-03-31 12:29:13 -0300

    Adicionado @EnableAsync

--- src/main/java/com/fiap/scheduling/SchedulingApplication.java
+++ src/main/java/com/fiap/scheduling/SchedulingApplication.java
@@ -2,8 +2,10 @@ package com.fiap.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SchedulingApplication {
 
 	public static void main(String[] args) {
